022463de126673da021df7488b09f16babbce017
Ryan Collingham <ryanc118@gmail.com>
2019-06-28 00:13:33 +0100

    Updates

---
+++ precache-manifest.a7ec006a70b425e561f8767e210d929e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8930e554c8f7872d8975d87ef90ec010",
+    "url": "/react-hangman-app/index.html"
+  },
+  {
+    "revision": "8a438bd61dbd1b7e5df0",
+    "url": "/react-hangman-app/static/css/main.edaf8e8e.chunk.css"
+  },
+  {
+    "revision": "99e7ec56baf70c56aa3b",
+    "url": "/react-hangman-app/static/js/2.6a01dcbd.chunk.js"
+  },
+  {
+    "revision": "8a438bd61dbd1b7e5df0",
+    "url": "/react-hangman-app/static/js/main.78ca44a5.chunk.js"
+  },
+  {
+    "revision": "a38d277fb0b5ae2864af",
+    "url": "/react-hangman-app/static/js/runtime~main.55837de3.js"
+  }
+]);
